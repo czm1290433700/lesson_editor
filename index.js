f2d156a99307c76dc59f42279b5835e5fad77fa0
czm <1290433700@qq.com>
2021-02-01 09:32:00 +0800

    捕获dom节点信息，并自动生成课程文本

--- index.js
+++ index.js
@@ -14,35 +14,37 @@ const legao = {
 // kitten课评库常量
 const kitten = { 
     content : [ 
-        `宝贝的课堂表现特别好，在拼搭积木的过程中，还能够自己仔细观察成品，并且自己动手拼搭，观察能力和动手能力都不错，还能够给自己的机器人拼出不一样的造型，比较有自己的想法，不错，继续加油，继续加油，老师期待你更好的作品。`,
-        `宝贝整堂课都表现得不错，很乖巧，能够积极回答老师的问题，在后面拼搭积木的过程中，也能够认真的观察成品，认真听老师讲课，自己动手一步步地把积木拼好了，很棒，在拼搭的过程中，还学会了固定积木的小技巧，最后分享的环节，也比较勇敢，棒棒哒，继续加油，老师期待你更好的作品。`,
-        `宝贝他的观察能力很好，也很聪明，能够很快知道需要用到什么积木。搭积木的时候，表现的也很好，搭建的速度很快，在等别的小朋友的时候，他能自己根据模型搭出下一步，动手能力很棒哦，喜欢的话来编程猫，学习更多有趣的知识哦。`,
+        `同学在整节课中表现得特别好，以前他没有接触过角色阵营，所以他在课堂上听得很认真，也可以看出他很感兴趣，听老师讲解完后，他兴致勃勃地自己开始实践，做得很好，不仅顺利完成了成果，还把第二个挑战任务也做出来了，很棒，继续加油哦，老师期待你更多优秀的作品。`,
+        `同学在课堂上很认真，他会对项目进项思考和实践，同时他也喜欢往项目中添加自己的元素进去，这样很好，会记得更清楚，棒棒哒，继续加油，老师期待你更多优秀的作品。`,
+        `同学在整节课中表现得特别好，很认真，他善于自己思考问题，老师提出问题之后，他会自己认真地思考，遇到解决不了的问题才会问老师，这样真棒，自己学得也快，记得也快，在错误中学习和成长，要继续加油，老师期待你更多优秀的作品。`,
         `宝贝在课堂上表现得很好，积极配合老师，回答老师的问题，真棒，另外，宝贝的观察能力和动手能力还是不错的，他能够通过观察成品想象出一个基本的形状，然后有目的地拼出相似的形状出来，当做出来的作品和成品有一些出入时，宝贝在老师的指导下也能够马上纠正过来，棒棒哒。继续加油，老师期待你更好的作品。`,
         `宝贝很棒，很乖巧，能够积极回答老师的问题，在后面拼搭积木的过程中，也能够认真的观察成品，认真听老师讲课，自己动手一步步地把积木拼好了，很棒，在拼搭的过程中，还学会了固定积木的小技巧，棒棒哒，继续加油，老师期待你更好的作品。`,
         `宝贝很棒，很乖巧，能够积极回答老师的问题，在拼搭积木的过程中，也能够自己动手一步步做好，非常棒，后面增加旋转的时候，能够自己修改不同的速度和时间，学得很好，棒棒哒，要继续加油，喜欢的话来编程猫，创作更多有意思的作品哦。`
     ],
-    head1 : `宝贝今天完成了`,
-    head2 : `的作品，课程的目标主要有以下几点：`
+    head1 : `同学今天顺利地完成了本节课`,
+    head2 : `的作品，这节课的主要知识点包括`
 }
 // python课评库常量
 const python = { 
     content : [ 
-        `宝贝的课堂表现特别好，在拼搭积木的过程中，还能够自己仔细观察成品，并且自己动手拼搭，观察能力和动手能力都不错，还能够给自己的机器人拼出不一样的造型，比较有自己的想法，不错，继续加油，继续加油，老师期待你更好的作品。`,
-        `宝贝整堂课都表现得不错，很乖巧，能够积极回答老师的问题，在后面拼搭积木的过程中，也能够认真的观察成品，认真听老师讲课，自己动手一步步地把积木拼好了，很棒，在拼搭的过程中，还学会了固定积木的小技巧，最后分享的环节，也比较勇敢，棒棒哒，继续加油，老师期待你更好的作品。`,
-        `宝贝他的观察能力很好，也很聪明，能够很快知道需要用到什么积木。搭积木的时候，表现的也很好，搭建的速度很快，在等别的小朋友的时候，他能自己根据模型搭出下一步，动手能力很棒哦，喜欢的话来编程猫，学习更多有趣的知识哦。`,
-        `宝贝在课堂上表现得很好，积极配合老师，回答老师的问题，真棒，另外，宝贝的观察能力和动手能力还是不错的，他能够通过观察成品想象出一个基本的形状，然后有目的地拼出相似的形状出来，当做出来的作品和成品有一些出入时，宝贝在老师的指导下也能够马上纠正过来，棒棒哒。继续加油，老师期待你更好的作品。`,
-        `宝贝很棒，很乖巧，能够积极回答老师的问题，在后面拼搭积木的过程中，也能够认真的观察成品，认真听老师讲课，自己动手一步步地把积木拼好了，很棒，在拼搭的过程中，还学会了固定积木的小技巧，棒棒哒，继续加油，老师期待你更好的作品。`,
-        `宝贝很棒，很乖巧，能够积极回答老师的问题，在拼搭积木的过程中，也能够自己动手一步步做好，非常棒，后面增加旋转的时候，能够自己修改不同的速度和时间，学得很好，棒棒哒，要继续加油，喜欢的话来编程猫，创作更多有意思的作品哦。`
+        `同学今天表现得依旧很好，他上课会认真听讲，不会做其他的事情，有的时候课间休息期间还会认真思考上节课的问题，还是很好学的，在写程序的过程中，遇到了任何问题他也会主动地问老师，很棒，这样学到的才更多，要继续加油哦，老师期待更好的你。`,
+        `同学是一个非常善于主动学习的学生，他会在老师讲课的时候认真思考，同时自己在自己的电脑上面尝试，自己去实践，在实践当中去记忆，这是一个很好的方法，很棒，要继续保持学习的热情哦。`,
+        `同学还是很聪明，总是在讲新知识点的时候能够积极主动地去记忆和尝试，很配合老师，非常好，这样学到地知识就更加牢固了，同时他也很有自己的编程想法，很棒，继续加油呀`,
+        `同学是一个非常善于主动学习的学生，他会在老师讲课的时候认真思考，同时自己在自己的电脑上面尝试，自己去实践，在实践当中去记忆，这是一个很好的方法，很棒，要继续保持学习的热情哦。`,
+        `同学在课堂上表现得很不错，他是一个很听话的学生，也比较善于思考，在接触新的概念的时候，他都能听得很认真，积极地去思考以及回答问题，很快就学会了一些新方法的基本使用，并且在创作作品的时候，还能够做出难度更高的挑战任务，很不错，希望持续保持对学习的热情哦`,
+        `同学的编程思想还是很不错的，在讲授新知识的时候，他还是能够比较轻松地理解，并且根据老师的示范，他也会自己做一遍代码实践，再加上自己的一些思考，这样学习会比较容易获得知识，加油，希望继续保持学习的积极态度哦。`
     ],
     head1 : `宝贝今天完成了`,
     head2 : `的作品，课程的目标主要有以下几点：`
 }
 
+const courses = ['乐高', 'kitten', 'python']
+
 /**
  * 课评头部
- * @param {*} name 
- * @param {*} title 
- * @param {*} mode 
+ * @param {*} name 学生姓名
+ * @param {*} title 课程名称
+ * @param {*} mode 课程种类，0乐高，1kitten，2python
  */
 const get_head = (name, title, mode) => {
     switch(mode) {
@@ -59,9 +61,8 @@ const get_head = (name, title, mode) => {
 
 /**
  * 课评内容
- * @param {*} name 
- * @param {*} title 
- * @param {*} mode 
+ * @param {*} name 学生姓名
+ * @param {*} mode 课程种类，0乐高，1kitten，2python
  */
 const get_content = (name, mode) => {
     let content = [];
@@ -78,7 +79,18 @@ const get_content = (name, mode) => {
         default:
             break;
     }
-    return `${name + content[Math.random() * content.length]}`;
+    return `${name + content[Math.floor(Math.random() * content.length)]}`;
+}
+
+/**
+ * 获得课评文本
+ * @param {*} stuName 
+ * @param {*} courseName 
+ * @param {*} courseGoal 
+ * @param {*} mode 
+ */
+const get_discuss = (stuName, courseName, courseGoal, mode) => {
+    return get_head(stuName, courseName, mode) + courseGoal + get_content(stuName, mode);
 }
 
 /**
@@ -86,8 +98,46 @@ const get_content = (name, mode) => {
  * @param {*} ev 
  */
 const addStuModule = (ev) => {
-    debugger
     ev = ev || window.event;
-    const obj = ev.srcElement || ev.target;
-    console.log(obj);
+    const obj = ev.srcElement || ev.target,
+        stus = obj.parentNode.querySelector('.course_stus'),
+        stu = document.createElement('div'),
+        input = document.createElement('input'),
+        label = document.createElement('label');
+    stu.className = 'course_module';
+    label.innerHTML = `学生姓名${stus.children.length + 1}：`;
+    stu.appendChild(label);
+    input.type = `text`;
+    input.name = `course_stuName`;
+    input.placeholder = `请输入学生姓名`;
+    stu.appendChild(input);
+    stus.appendChild(stu);
+}
+
+/**
+ * 生成课评
+ */
+const getCourseDiscuss = () => {
+    const formDatas = document.getElementsByClassName('course_form');
+    for(let i = 0; i < formDatas.length; i++) {
+        const formData = formDatas[i],
+            courseName = formData.querySelector('input[name="course_name"]').value, // 课程名称
+            courseGoal = formData.querySelector('textarea[name="course_goal"]').value,  // 课程目标
+            stuNames = formData.querySelectorAll('input[name="course_stuName"]'),
+            stus = [];  // 学生姓名数组
+        for(let stu of stuNames) {
+            const val = stu.value;
+            val ? stus.push(val) : '';
+        }
+        if(!courseName || !courseGoal || stus.length == 0) {
+            if(confirm(`${courses[i]}课程名称/课程目的/学生姓名未填, 将不生成该课程课评, 是否确认继续操作`)) {
+                continue ;
+            }else {
+                return;
+            }
+        }
+        for(let stu of stus) {
+            console.log(get_discuss(stu, courseName, courseGoal, i));
+        }
+    }
 }
